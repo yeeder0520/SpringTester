e05dd6323b11add420e41cc34092362b3bdc0b7b
yeeder0520 <59109960+yeeder0520@users.noreply.github.com>
2023-04-27 11:36:54 +0800

    add main

---
+++ src/main/java/com/example/demo/config/TestGit.java
@@ -0,0 +1,11 @@
+package com.example.demo.config;
+
+/**
+ * @author YeeDer
+ * @since 2023/4/27 AM 11:35
+ **/
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("args = " + args);
+    }
+}
