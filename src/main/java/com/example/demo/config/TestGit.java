6ce610081505c167b981f172de6cf55c2ac735bf
yeeder0520 <59109960+yeeder0520@users.noreply.github.com>
2023-04-27 11:54:30 +0800

    Revert "Revert "Revert "add 789"""
    This reverts commit 075980ed4c9720b3dee228933788a69d2cd4594f.

--- src/main/java/com/example/demo/config/TestGit.java
+++ src/main/java/com/example/demo/config/TestGit.java
@@ -7,5 +7,9 @@ package com.example.demo.config;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("args = " + args);
+
+//        123
+//        456
+//        789
     }
 }
